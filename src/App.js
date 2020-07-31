dd3b3d606ba5bcf7590a321bfc15df5505358260
Wei Lu <55832638+Rainiereed@users.noreply.github.com>
2020-07-31 20:42:17 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import Slideshow from "./components/Slideshow";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Slideshow />
+    </div>
+
+  );
+}
+
+export default App;
