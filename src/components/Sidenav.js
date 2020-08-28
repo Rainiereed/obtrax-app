ab3065d88ff7c33ad80e8dbf64904cb519c9fcac
Wei Lu <55832638+Rainiereed@users.noreply.github.com>
2020-08-28 09:20:45 +0200

    side navigation

---
+++ src/components/Sidenav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import burger from '../assets/burger.png';
+import cross from '../assets/cross.png';
+import "./styles/Sidenav.scss";
+
+class Sidenav extends React.Component {
+    showSettings (event) {
+      event.preventDefault();
+     
+    }
+  
+    render () {
+      
+      return (
+        <Menu disableAutoFocus width={ '95%' } customBurgerIcon={ <img src={burger} alt="burgericon"/> } customCrossIcon={ <img src={cross} alt="crossicon"/> }>
+          
+          <a id="timer" className="menu-item" href="/timer">TIMER</a>
+          <a id="minprofil" className="menu-item" href="/minprofile">MIN PROFIL</a>
+          <a id="inställningar" className="menu-item" href="/inställningar">INSTÄLLNINGAR</a>
+          <a id="viktiginformation" className="menu-item" href="/viktiginformation">VIKTIG INFORMATION</a>
+          <a id="historik" className="menu-item" href="/historik">HISTORIK</a>
+          
+        </Menu>
+      );
+    }
+  }
+
+export default Sidenav;
