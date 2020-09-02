927576e6865ffb64526be2eec3121b58c28a36a8
Wei Lu <55832638+Rainiereed@users.noreply.github.com>
2020-09-03 00:17:25 +0200

    Logo Button component

---
+++ src/components/LogoButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Popup from "../pages/Popup";
+import "./styles/LogoButton.scss";
+
+const LogoButton = () => {
+    return (
+        <Popup />
+    )
+}
+
+
+export default LogoButton;
