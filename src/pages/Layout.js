fa6f5884f40a250194fb5344721a4975ad15cb68
Wei Lu <55832638+Rainiereed@users.noreply.github.com>
2020-09-21 22:54:20 +0200

    Update Layout.js

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -5,17 +5,19 @@ import {
     Route,
 } from "react-router-dom";
 
-import Slideshow from "../components/Slideshow";
+import Onboarding from "../pages/Onboarding";
 import Popup from "../pages/Popup";
 import PopupReminder from "../pages/PopupReminder";
 import Painintensity from "../pages/Painintensity";
 import CallMidwife from "../pages/CallMidwife";
-import Contraction from "../components/Contraction";
+import Contraction from "../pages/Contraction";
+import ContractionSummary from '../pages/ContractionSummary';
 import Minprofil from "../pages/Minprofil";
 import Settings from "../pages/Settings";
 import Information from "../pages/Information";
 import RegisterSymptoms from "../pages/RegisterSymptoms";
 import WaterBroken from "../pages/WaterBroken";
+import RegisteredConfirm from "../pages/RegisteredConfirm";
 
 
 export default function Layout() {
@@ -23,7 +25,7 @@ export default function Layout() {
         <div>
             <Router>
                 <Switch>
-                    <Route exact path="/" component={Slideshow} />
+                    <Route exact path="/" component={Onboarding} />
                     <Route exact path="/ring" component={CallMidwife} />
                     <Route exact path="/popup" component={Popup} />
                     <Route exact path="/reminder" component={PopupReminder} />
@@ -31,8 +33,10 @@ export default function Layout() {
                     <Route exact path="/minprofile" component={Minprofil} />
                     <Route exact path="/setting" component={Settings} />
                     <Route exact path="/information" component={Information} />
+                    <Route exact path="/pain" component={Painintensity} />
                     <Route exact path="/symptoms" component={RegisterSymptoms} />
                     <Route exact path="/water" component={WaterBroken} />
+                    <Route exact path="/confirm" component={RegisteredConfirm} />
                 </Switch>
             </Router>
         </div>
