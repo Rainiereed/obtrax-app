569db6d5f6a737c457c6ae74807d96572a004a0c
Wei Lu <55832638+Rainiereed@users.noreply.github.com>
2020-09-24 23:35:52 +0200

    Update RegisterSymptoms.js

--- src/pages/RegisterSymptoms.js
+++ src/pages/RegisterSymptoms.js
@@ -9,7 +9,7 @@ const Symptoms = () => {
     return (
         <div>
             <div className="closebutton">
-            <img src={cross} alt="crossicon" />
+                <img src={cross} alt="crossicon" />
             </div>
             <div>
                 <LogoButton />
@@ -17,24 +17,19 @@ const Symptoms = () => {
             <br></br>
             <h1>Registrera symptom</h1>
             <br></br>
-            <br></br>
             <div className="symptomCard">
                 <br></br>
                 <p>Beskriv dina symptom</p>
-                <br></br>
                 <WaterBrokenButton className="symptomButton" id="Item1" path="/water" text="Vattnet har gått" />
-                <br></br>
-                <br></br>
-                <button className="symptomButton" id="Item2">Jag blöder</button>
-                <br></br>
-                <br></br>
-                <button className="symptomButton" id="Item3">Slemproppen har gått</button>
-                <br></br>
-                <br></br>
-                <button className="symptomButton" id="Item4">Jag känner mig sjuk</button>
+                <br></br>               
+                <div className="symptomButton" id="Item2">Jag blöder</div>
+                <br></br>              
+                <div className="symptomButton" id="Item3">Slemproppen har gått</div>
+                <br></br>              
+                <div className="symptomButton" id="Item4">Jag känner mig sjuk</div>
             </div>
             <div>
-            <br></br>
+                <br></br>
                 <p>Känner du dig osäker? </p>
             </div>
             <div>
