a8bef5d25dd86d175515a1c808627039847a56d7
Wei Lu <55832638+Rainiereed@users.noreply.github.com>
2020-09-25 12:07:32 +0200

    Update Onboarding.js

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
+import LogoButton from "../components/LogoButton"
 import 'react-slideshow-image/dist/styles.css';
 import "../pages/Onboarding.scss";
-import LogoButton from "../components/LogoButton"
 import icon1 from '../assets/icon1.png';
 import icon2 from '../assets/icon2.png';
 import icon3 from '../assets/icon3.png';
@@ -18,13 +18,13 @@ const properties = {
 const Onboarding = () => {
   return (
     <div>
-      <LogoButton />
-      <div className="slide-container">
+      <LogoButton /> 
+      <div className="slide-container nav span">
 
         <Slide {...properties}>
           <div className="each-slide">
-
-            <h1>Välkommen</h1>
+          
+          <h1>Välkommen</h1> 
             <h3>Låt oss presentera OB Trax - din bästa digitala vän för er, dig och ditt barn! Vi håller koll på hur ni mår och att allt står rätt till.</h3>
 
             <br></br>
@@ -37,7 +37,7 @@ const Onboarding = () => {
           </div>
           <div className="each-slide">
 
-            <h1>Bättre koll på er</h1>
+           <h1>Bättre koll på er</h1> 
             <h3>OB Trax analyserar dina värkar och uppdaterar sjukhuset för att du ska känna dig trygg oavsett i vilken vecka du och ditt barn befinner er.</h3>
 
             <br></br>
@@ -50,7 +50,7 @@ const Onboarding = () => {
           </div>
           <div className="each-slide">
 
-            <h1>Värdefull information</h1>
+            <h1>Värdefull information</h1> 
             <h3>Klicka på <strong><a href="/minprofile">Lägg till info</a></strong> för att ge OB Trax värdefull information som tillgängliggör hjälp till er bäst på bästa sätt!<br>
             </br>
               <br></br>
